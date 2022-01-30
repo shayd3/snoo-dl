c57083378d54f79a0399fd247bd0bacda1bbab4d
shayd3 <ryan.ross215@gmail.com>
2022-01-30 11:28:56 -0500

    Add resolution and aspect-ratio flags. Extend Post struct to include source image dimensions. Add Filter

--- models/reddit_post.go
+++ models/reddit_post.go
@@ -5,5 +5,14 @@ type Post struct {
 	Data struct {
 		Title string `json:"title"`
 		Url string `json:"url"`
+		Preview struct {
+			Images  []struct {
+				Source struct {
+					Height int64  `json:"height"`
+					URL    string `json:"url"`
+					Width  int64  `json:"width"`
+				} `json:"source"`
+			} `json:"images"`
+		} `json:"preview"`
 	}`json:"data"`
 }
